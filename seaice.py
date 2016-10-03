9101da87b47466213080eb2ab8d1401f25f270a9
Mark Morgan <momipsl@ipsl.jussieu.fr>
2016-10-03 13:22:38 +0200

    Corrected issue with change history

--- seaice.py
+++ seaice.py
@@ -31,7 +31,7 @@ CONTRIBUTORS = '&lt;NEED CONTRIBUTORS&gt;'
 # --------------------------------------------------------------------
 CHANGE_HISTORY = [
 	("0.1.0", "2016-05-24", "Initialised", "David Hassell"),
-	("0.1.1", "2016-09-28", "Modified seaice_grid", "Ruth Petrie")
+	("0.1.1", "2016-09-28", "Modified seaice_grid", "Ruth Petrie"),
 	("0.1.1", "2016-09-28", "Added seaice_key_properties.py as a copy from ocean MUST update for sea ice", "Ruth Petrie")	
      ]
 
