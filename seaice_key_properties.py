b74d651c219cfddfc8eeba92a4a831f00e0a9702
Ruth Petrie <ruth.petrie@stfc.ac.uk>
2016-11-11 13:23:32 +0000

    Refactoring

--- seaice_key_properties.py
+++ seaice_key_properties.py
@@ -13,7 +13,7 @@ ENUMERATIONS = OrderedDict()
 # Default process details pulled from CIM.
 DETAILS['CIM'] = {
     'description': 'Key properties of the sea ice component',
-    'properties':[
+    'properties': [
         ('implementation_overview','str', '1.1',
             "General overview description of the implementation of this part of the process."),
         ('keywords','str', '0.N',
