c339303b384176e20c658debd29f8627ba000ab9
RuthPetrie <ruth.petrie@stfc.ac.uk>
2017-02-13 20:44:20 +0000

    Add short table

--- seaice_short_table.py
+++ seaice_short_table.py
@@ -24,6 +24,7 @@
             "cmip6.seaice.thermodynamics.energy.enthalpy_formulation",
             "cmip6.seaice.thermodynamics.mass.new_ice_formation",
             "cmip6.seaice.thermodynamics.salt.mass_transport.salinity_type",
+            "cmip6.seaice.thermodynamics.salt.thermodynamics.salinity_type",
             "cmip6.seaice.thermodynamics.melt_ponds.formulation",
             "cmip6.seaice.radiative_processes.ice_radiation_transmission",
         ],
